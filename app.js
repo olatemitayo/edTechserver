07a6bfc43dfd4343a9cbffca2b3ac4b9ac2d2a8f
Temitayo Olatunji <olatunjitemitayo444@gmail.com>
2022-11-29 20:05:37 +0100

    firstcommit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const router = require("./routes/user-routes");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cookieParser());
@@ -14,7 +15,7 @@ mongoose
     "mongodb+srv://edtech:redred1234@cluster0.u95hdad.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database is connected to port 5000");
   })
   .catch((err) => console.log(err));
